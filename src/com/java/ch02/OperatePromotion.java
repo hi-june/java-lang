8c075ebb9ea4455fff4b37b80c3ff1e8aacedcae
hi-june <wnsgur735@gmail.com>
2022-09-29 20:02:55 +0900

    Chapter 02 - 변수와 자료형

---
+++ src/com/java/ch02/OperatePromotion.java
@@ -0,0 +1,12 @@
+package com.java.ch02;
+
+public class OperatePromotion {
+    public static void main(String[] args) {
+        short num1 = 11;
+        short num2 = 22;
+
+        //short result = num1 + num2; // 컴파일 오류 발생
+
+        //System.out.println(result);
+    }
+}
