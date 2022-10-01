b48e4940626eece71c6ed765f240341660302a92
hi-june <wnsgur735@gmail.com>
2022-10-01 14:39:55 +0900

    Chapter 04 - 연산자(Operators)

---
+++ src/com/java/ch04/SCE.java
@@ -0,0 +1,19 @@
+package com.java.ch04;
+
+public class SCE {
+    public static void main(String[] args) {
+        int num1 = 0;
+        int num2 = 0;
+        boolean result;
+
+        result = ((num1 += 10)<0) && ((num2 += 10) > 0);    // 앞 조건문에서 false를 반환하고, and 연산이 수행되기 때문에 뒷 연산이 생략된다.
+        System.out.println("result = " + result);
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2 + '\n');
+
+        result = ((num1 += 10) > 0) || ((num2 += 10) > 0);  // 앞 조건문에서 true를 반환하고, or 연산이 수행되기 때문에 뒷 연산이 생략된다.
+        System.out.println("result = " + result);
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+    }
+}
