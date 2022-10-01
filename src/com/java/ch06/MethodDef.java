2870e95409db381f91d6e4402f2604ddf745a6ac
hi-june <wnsgur735@gmail.com>
2022-10-01 21:20:56 +0900

    Chapter 06 - 메소드와 변수의 스코프

---
+++ src/com/java/ch06/MethodDef.java
@@ -0,0 +1,14 @@
+package com.java.ch06;
+
+public class MethodDef {
+    public static void main(String[] args) {
+        System.out.println("프로그램의 시작");
+        hiEveryone(26);
+        System.out.println("프로그램의 끝");
+    }
+
+    public static void hiEveryone(int age) {
+        System.out.println("좋은 아침입니다.");
+        System.out.println("제 나이는 " + age + "입니다.");
+    }
+}
