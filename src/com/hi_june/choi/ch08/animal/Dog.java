bf2fb98889ee3dbb282d321230b0766569077606
hi-june <wnsgur735@gmail.com>
2022-10-10 02:26:52 +0900

    package changed

--- src/com/hi_june/choi/ch08/animal/Dog.java
+++ src/com/hi_june/choi/ch08/animal/Dog.java
@@ -1,12 +1,12 @@
-package animal;
+package com.hi_june.choi.ch08.animal;
 
 public class Dog {
     public static void main(String[] args) {
-        zoo.Cat cat = new zoo.Cat();
+        com.hi_june.choi.ch08.zoo.Cat cat = new com.hi_june.choi.ch08.zoo.Cat();
         welcome(cat);
     }
 
-    public static void welcome(zoo.Cat cat){
+    public static void welcome(com.hi_june.choi.ch08.zoo.Cat cat){
         cat.makeSound();    // 호출 가능! 컴파일 성공!
         // cat.makeHappy(); // 호출 불가! 컴파일 오류!
     }
--- src/com/hi_june/choi/ch08/zoo/Cat.java
+++ src/com/hi_june/choi/ch08/zoo/Cat.java
@@ -1,4 +1,4 @@
-package zoo;
+package com.hi_june.choi.ch08.zoo;
 
 public class Cat {
     public void makeSound() {   // public 메소드, 어디서든 호출 가능
